eda8ffe007152c9c3f5776ce598b6f12fe21ab42
Alex Strugacz <alexstrugacz@icloud.com>
2024-06-27 09:16:30 -0500

    fix routing on homepage

--- NormandyAppProject/normandy_app/lib/src/homePage.dart
+++ NormandyAppProject/normandy_app/lib/src/homePage.dart
@@ -9,52 +9,42 @@ class HomePage extends StatelessWidget {
     'Projects Dashboard',
     'My Profile',
   ];
+
+  final Map<String, String> buttonRoutes = {
+    'My Profile': '/profile',
+    'Quick Links': '/quick-links',
+    'Contacts': '/business-contacts-list'
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('HomePage'),
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SvgPicture.asset(
-                'assets/icon.svg',
-                height: 80,
-                width: 80,
-              ),
+      appBar: AppBar(
+        title: Text('HomePage'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: SvgPicture.asset(
+              'assets/icon.svg',
+              height: 80,
+              width: 80,
             ),
-            // Header
-            const Text(
-              'Mon. Aug 17',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
+          ),
+          // Header
+          const Text(
+            'Mon. Aug 17',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
             ),
-            SizedBox(height: 16),
+          ),
+          SizedBox(height: 16),
 
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/profile');
-                },
-                child: Text('My Profile')),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/quick-links');
-              },
-              child: Text('Go to Quick Links'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/business-contacts-list');
-              },
-              child: Text('Business Contacts'),
-            ),
-            // Grid of buttons
-            Expanded(
-                child: Padding(
+          // Grid of buttons
+          Expanded(
+            child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: GridView.count(
                 crossAxisCount: 2,
@@ -63,14 +53,18 @@ class HomePage extends StatelessWidget {
                 children: List.generate(buttonNames.length, (index) {
                   return ElevatedButton(
                     onPressed: () {
-                      //Route to the corresponding page
-
+                      // Route to the corresponding page if the route exists
+                      if (buttonRoutes.containsKey(buttonNames[index])) {
+                        Navigator.pushNamed(
+                            context, buttonRoutes[buttonNames[index]]!);
+                      } else {
+                        // Handle case where the route is not defined
+                        print('No route defined for ${buttonNames[index]}');
+                      }
                       print('Pressed ${buttonNames[index]}');
                     },
                     style: ElevatedButton.styleFrom(
-                      // Align text to top-left of button
                       alignment: Alignment.topLeft,
-                      // Add padding for better alignment
                       padding: EdgeInsets.all(16.0),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10.0),
@@ -79,12 +73,14 @@ class HomePage extends StatelessWidget {
                     child: Text(
                       buttonNames[index],
                       style: TextStyle(fontWeight: FontWeight.normal),
-                    ), // Display actual name
+                    ),
                   );
                 }),
               ),
-            )),
-          ],
-        ));
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
