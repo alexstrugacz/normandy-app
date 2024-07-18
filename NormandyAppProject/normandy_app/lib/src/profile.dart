781abac4fc2b232f458ee366cadde077d799cfa8
James <jamesrupbtan@gmail.com>
2024-07-18 09:08:26 -0500

    Implement "Clear cache instructions" functionality

--- NormandyAppProject/normandy_app/lib/src/profile.dart
+++ NormandyAppProject/normandy_app/lib/src/profile.dart
@@ -1,8 +1,8 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 import 'components/grid_card.dart';
 
@@ -26,7 +26,7 @@ class Profile extends StatelessWidget {
                 final SharedPreferences prefs =
                     await SharedPreferences.getInstance();
                 await prefs.setStringList('favoriteContacts', []);
-                showAlertDialog(context);
+                if (context.mounted) showAlertDialog(context);
               },
             ),
             GridCard(
@@ -36,19 +36,16 @@ class Profile extends StatelessWidget {
                 final SharedPreferences prefs =
                     await SharedPreferences.getInstance();
                 await prefs.remove('jwt');
-                if (context.mounted) Navigator.pushNamedAndRemoveUntil(context, "/", (r) => false);
+                if (context.mounted) {
+                  Navigator.pushNamedAndRemoveUntil(context, "/", (r) => false);
+                }
               },
             ),
             GridCard(
               icon: FontAwesomeIcons.file,
               text: "Clear OneDrive Cache",
               onTap: () async {
-                // The Cache is local? Because of App sandboxing, I am not sure if we can do this directly.
-                // TODO link to the appropriate page in the Settings app, so users can clear it there.
-                String url = (Theme.of(context).platform == TargetPlatform.android)
-                  ? 'package:com.microsoft.skydrive'
-                  : 'app-settings:';
-                await launchUrlString(url);
+                if (context.mounted) showCacheInstructions(context);
               },
             ),
           ]),
@@ -56,24 +53,94 @@ class Profile extends StatelessWidget {
   }
 }
 
-showAlertDialog(BuildContext context) {
-
+showCacheInstructions(BuildContext context) {
   // set up the button
   Widget okButton = TextButton(
     child: const Text("OK"),
-    onPressed: () { 
+    onPressed: () {
       Navigator.of(context).pop();
     },
   );
 
   // set up the AlertDialog
   AlertDialog alert = AlertDialog(
-    title: const Padding(padding: EdgeInsets.only(top: 8, left: 8, right: 8), child: Text("Favorites cleared")),
-    content: const Padding(padding: EdgeInsets.only(top: 0, left: 8, right: 8), child: Text("Your favorites have been cleared.")),
-    actions: [
-      okButton,
-    ]
+      title: const Padding(
+          padding: EdgeInsets.only(top: 8, left: 8, right: 8),
+          child: Text("How to Clear your OneDrive Cache")),
+      content: Padding(
+        padding: const EdgeInsets.only(top: 0, left: 8, right: 8),
+        child: Platform.isAndroid
+            ? SingleChildScrollView(
+                child: ListBody(
+                  children: const <Widget>[
+                    Text("Android instructions",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text("1. Open Settings"),
+                    Text("2. Select Applications, then Manage Applications"),
+                    Text("3. Click OneDrive"),
+                    Text("4. Inside this OneDrive page:"),
+                    Text("    a. Tap Force Stop"),
+                    Text("    b. Tap Clear data"),
+                    Text("    c. Tap Clear cache"),
+                    Text("5. Uninstall OneDrive"),
+                    Text("6. Re-install OneDrive from Google Play"),
+                  ],
+                ),
+              )
+            : SingleChildScrollView(
+                child: ListBody(
+                  children: const <Widget>[
+                    Text("iOS instructions",
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text("1. Uninstall the OneDrive app from your phone."),
+                    Text("    a. Long press on the App on your Home screen."),
+                    Text("    b. Tap the X, and confirm to uninstall."),
+                    Text(
+                        "2. Soft reset your phone by holding the Power and Home button simultaneously."),
+                    Text(
+                        "    a. If your phone does not have a Home button, instead press and release the Volume Up button"),
+                    Text(
+                        "    b. Then press and release the Volume Down button"),
+                    Text(
+                        "    c. Press and hold the Power button until you see the Apple logo"),
+                    Text("3. Re-install OneDrive from App Store"),
+                  ],
+                ),
+              ),
+      ),
+      actions: [
+        okButton,
+      ]);
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
   );
+}
+
+showAlertDialog(BuildContext context) {
+  // set up the button
+  Widget okButton = TextButton(
+    child: const Text("OK"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+      title: const Padding(
+          padding: EdgeInsets.only(top: 8, left: 8, right: 8),
+          child: Text("Favorites cleared")),
+      content: const Padding(
+          padding: EdgeInsets.only(top: 0, left: 8, right: 8),
+          child: Text("Your favorites have been cleared.")),
+      actions: [
+        okButton,
+      ]);
 
   // show the dialog
   showDialog(
