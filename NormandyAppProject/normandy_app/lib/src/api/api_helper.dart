a8c1ed64e2bcf839df3a62a3975a9b6f0b232035
miguelaenlle <bot.developer3@gmail.com>
2024-07-18 10:06:35 -0500

    Switched back to normandy backend

--- NormandyAppProject/normandy_app/lib/src/api/api_helper.dart
+++ NormandyAppProject/normandy_app/lib/src/api/api_helper.dart
@@ -5,8 +5,10 @@ import 'package:http/http.dart' as http;
 
 
 class APIHelper {
-  // static String baseUrl = "https://normandy-backend.azurewebsites.net/api/";
-  static String baseUrl = "http://localhost:4000/api/";
+  static String baseUrl = "https://normandy-backend.azurewebsites.net/api/";
+
+  // Only uncomment this in development, and ensure your NormandyBackend copy is running on Port 4000
+  // static String baseUrl = "http://localhost:4000/api/";
 
   static Future<http.Response?> get(String url, BuildContext context, bool mounted) async {
     String? jwt = await getJwt();
