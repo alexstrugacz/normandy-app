d788b38da546447d12828ea16302ec248c67ab7c
James <jamesrupbtan@gmail.com>
2025-01-10 14:23:51 -0600

    Open Teams links in external app

--- NormandyAppProject/normandy_app/lib/src/employee-list/launch_teams.dart
+++ NormandyAppProject/normandy_app/lib/src/employee-list/launch_teams.dart
@@ -2,8 +2,9 @@ import 'package:url_launcher/url_launcher.dart';
 
 handleLaunchTeamsCall(String email) async {
   var url = Uri.parse("https://teams.microsoft.com/l/call/0/0?users=$email");
-  if(await canLaunchUrl(url)) {
-    await launchUrl(url);
+  if (await canLaunchUrl(url)) {
+    // if this doesn't work, try LaunchMode.externalNonWebApplication
+    await launchUrl(url, mode: LaunchMode.externalApplication);
   } else {
     throw "Failed to make call";
   }
@@ -11,8 +12,8 @@ handleLaunchTeamsCall(String email) async {
 
 handleLaunchTeamsMessage(String email) async {
   var url = Uri.parse("https://teams.microsoft.com/l/chat/0/0?users=$email");
-  if(await canLaunchUrl(url)) {
-    await launchUrl(url);
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url, mode: LaunchMode.externalApplication);
   } else {
     throw "Failed to make call";
   }
