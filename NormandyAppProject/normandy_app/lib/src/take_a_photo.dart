5d9ca6d2ae0b464514bfeb4c35f4ab7e2abcf379
Jibbie0 <halaswolff07@gmail.com>
2025-02-21 19:12:43 -0600

    Reverted env file to original

--- NormandyAppProject/normandy_app/lib/src/take_a_photo.dart
+++ NormandyAppProject/normandy_app/lib/src/take_a_photo.dart
@@ -59,9 +59,9 @@ class TakeAPhotoState extends State<TakeAPhoto> {
 
   void _initializeEnvVariables() {
     try {
-      clientId = clientId;
-      clientSCRT = clientSecret;
-      tenantId = tenantId;
+      clientId = CLIENT_ID;
+      clientSCRT = CLIENT_SCRT;
+      tenantId = TENANT_ID;
     } catch (e) {
       if(kDebugMode) print('Error accessing environment variables: $e');
     }
