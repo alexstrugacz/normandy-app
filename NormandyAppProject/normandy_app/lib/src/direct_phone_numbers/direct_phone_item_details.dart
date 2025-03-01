8adcfc9b1e3ffc281eceb3c52aceb31d59ba84c6
James <jamesrupbtan@gmail.com>
2025-02-28 21:50:15 -0600

    Allow clicking info in Contacts

--- NormandyAppProject/normandy_app/lib/src/direct_phone_numbers/direct_phone_item_details.dart
+++ NormandyAppProject/normandy_app/lib/src/direct_phone_numbers/direct_phone_item_details.dart
@@ -31,7 +31,8 @@ class DirectPhoneItemDetailsState extends State<DirectPhoneItemDetails> {
   }
 
   void loadContactIsFavorite() async {
-    bool currentlyIsFavorite = await checkIsFavorite(widget.person.id, 'directPhones');
+    bool currentlyIsFavorite =
+        await checkIsFavorite(widget.person.id, 'directPhones');
     setState(() {
       isFavorite = currentlyIsFavorite;
     });
@@ -40,180 +41,199 @@ class DirectPhoneItemDetailsState extends State<DirectPhoneItemDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("${widget.person.lastName}, ${widget.person.firstName}"),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context, true);
-          },
+        appBar: AppBar(
+          title: Text("${widget.person.lastName}, ${widget.person.firstName}"),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context, true);
+            },
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 48),
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 40,
-                child: Text(
-                  widget.person.initials,
-                  style: TextStyle(fontSize: 24),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Text(
-                "${widget.person.firstName} ${widget.person.lastName}",
-                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                '${widget.person.jobTitle} - ${widget.person.department}',
-                style: const TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-              const Text(
-                'Normandy Remodeling',
-                style: TextStyle(fontSize: 18, color: Colors.grey),
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.call),
-                    onPressed: () {
-                      handlePhoneCall(widget.person.cellPhone);
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.message),
-                    onPressed: () {
-                      handleMessage(widget.person.cellPhone);
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.email),
-                    onPressed: () {
-                      handleEmail(widget.person.email);
-                    },
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.star),
-                    color: isFavorite
-                          ? const Color.fromARGB(255, 221, 150, 8)
-                          : const Color.fromARGB(255, 80, 80, 80),
-                    onPressed: () {
-                      toggleFavorite();
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              GestureDetector(
-                onTap: () => {
-                  handleLaunchTeamsCall(widget.person.email)
-                },
-                child: Container(
-                padding: const EdgeInsets.all(8.0),
-                color: Colors.grey[200],
-                width: MediaQuery.of(context).size.width,
+        body: SingleChildScrollView(
+            child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 16, right: 16, top: 16, bottom: 48),
                 child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset('assets/images/teamsLogo.png', width: 20, height: 20, fit: BoxFit.cover,),
-                          const SizedBox(width: 5),
-                          const Text(
-                            'Call via Teams',
-                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ]
+                  children: [
+                    CircleAvatar(
+                      radius: 40,
+                      child: Text(
+                        widget.person.initials,
+                        style: TextStyle(fontSize: 24),
                       ),
-                      Text('${widget.person.firstName} ${widget.person.lastName}'),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 8),
-              GestureDetector(
-                onTap: () => {
-                  handleLaunchTeamsMessage(widget.person.email)
-                },
-                child: Container(
-                padding: const EdgeInsets.all(8.0),
-                color: Colors.grey[200],
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset('assets/images/teamsLogo.png', width: 20, height: 20, fit: BoxFit.cover,),
-                          const SizedBox(width: 5),
-                          const Text(
-                            'Message via Teams',
-                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ]
-                      ),
-                      Text('${widget.person.firstName} ${widget.person.lastName}'),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 8),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                color: Colors.grey[200],
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Phone',
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
-                    Text(widget.person.cellPhone),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 8),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                color: Colors.grey[200],
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Email',
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    Text(widget.person.email),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 8),
-              Container(
-                padding: const EdgeInsets.all(8.0),
-                color: Colors.grey[200],
-                width: MediaQuery.of(context).size.width,
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
+                    const SizedBox(height: 16),
                     Text(
-                      'Company',
-                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      "${widget.person.firstName} ${widget.person.lastName}",
+                      style: const TextStyle(
+                          fontSize: 24, fontWeight: FontWeight.bold),
                     ),
-                    Text('Normandy Remodeling'),
-                    Text('440 E Ogden Ave, Hinsdale, IL 60521'),
+                    Text(
+                      '${widget.person.jobTitle} - ${widget.person.department}',
+                      style: const TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                    const Text(
+                      'Normandy Remodeling',
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.call),
+                          onPressed: () {
+                            handlePhoneCall(widget.person.cellPhone);
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.message),
+                          onPressed: () {
+                            handleMessage(widget.person.cellPhone);
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.email),
+                          onPressed: () {
+                            handleEmail(widget.person.email);
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.star),
+                          color: isFavorite
+                              ? const Color.fromARGB(255, 221, 150, 8)
+                              : const Color.fromARGB(255, 80, 80, 80),
+                          onPressed: () {
+                            toggleFavorite();
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+                    GestureDetector(
+                      onTap: () => {handleLaunchTeamsCall(widget.person.email)},
+                      child: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        color: Colors.grey[200],
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(children: [
+                              Image.asset(
+                                'assets/images/teamsLogo.png',
+                                width: 20,
+                                height: 20,
+                                fit: BoxFit.cover,
+                              ),
+                              const SizedBox(width: 5),
+                              const Text(
+                                'Call via Teams',
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                            ]),
+                            Text(
+                                '${widget.person.firstName} ${widget.person.lastName}'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    GestureDetector(
+                      onTap: () =>
+                          {handleLaunchTeamsMessage(widget.person.email)},
+                      child: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        color: Colors.grey[200],
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(children: [
+                              Image.asset(
+                                'assets/images/teamsLogo.png',
+                                width: 20,
+                                height: 20,
+                                fit: BoxFit.cover,
+                              ),
+                              const SizedBox(width: 5),
+                              const Text(
+                                'Message via Teams',
+                                style: TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                            ]),
+                            Text(
+                                '${widget.person.firstName} ${widget.person.lastName}'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    GestureDetector(
+                      onTap: () {
+                        handlePhoneCall(widget.person.cellPhone);
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        color: Colors.grey[200],
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Phone',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            Text(widget.person.cellPhone),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    GestureDetector(
+                      onTap: () {
+                        handleEmail(widget.person.email);
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(8.0),
+                        color: Colors.grey[200],
+                        width: MediaQuery.of(context).size.width,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Email',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            Text(widget.person.email),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      color: Colors.grey[200],
+                      width: MediaQuery.of(context).size.width,
+                      child: const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Company',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text('Normandy Remodeling'),
+                          Text('440 E Ogden Ave, Hinsdale, IL 60521'),
+                        ],
+                      ),
+                    ),
                   ],
-                ),
-              ),
-            ],
-          )
-        )
-      )
-    );
+                ))));
   }
 }
